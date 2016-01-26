fc2ef0c3f4b97d59d26b62136bc769ba65e4faf0
System Administrator <root@Percivals-MBP.br.ibm.com>
2016-01-26 18:04:52 -0200

    v1

---
+++ timetable.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import datetime
+
+today = datetime.datetime.now()
+str_today = str(today.day).zfill(2) + "/" + str(today.month).zfill(2) + "/" + str(today.year)
+start_hour  = "09:00"
+number_hours = "9"
+
+if(today.weekday()<6):
+	driver = webdriver.Chrome()
+	driver.get("https://docs.google.com/forms/d/1bnbH5z-qV_LCsg5I_mdkd1T5kK0L3UF5T3MFbwXaCYs/viewform?edit_requested=true#")
+	#assert "Python" in driver.title
+	element = driver.find_element_by_id("entry_444885743")
+	element.send_keys(str_today)
+	element = driver.find_element_by_id("entry_645594624")
+	element.send_keys(start_hour)
+	element = driver.find_element_by_id("entry_235408508")
+	element.send_keys(number_hours)
+	elem = driver.find_element_by_id("ss-submit")
+	elem.send_keys(Keys.RETURN)
+	driver.close()
+	print("Enviado")
+
